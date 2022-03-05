8c679f7cf5b05fbb640dab8b902e728f6e9de2c8
Bara M <marconova.bara@gmail.com>
2022-03-05 10:10:22 +0100

    Ukol c.1

---
+++ ukol-1.py
@@ -0,0 +1,16 @@
+baliky = {
+    "B541X": True,
+    "B547X": False,
+    "B251X": False,
+    "B501X": True,
+    "B947X": False,
+}
+kod_baliku = input("Napis kod tveho baliku:")
+if kod_baliku in baliky:
+  if baliky[kod_baliku] == True:
+    print("Balík byl předán kurýrovi.")
+  else:
+    print("Balík zatím nebyl předán kurýrovi.")
+else:
+  print("Cislo baliku neni v databazi.")
+
